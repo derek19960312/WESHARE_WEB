31059a7791d5745aa92c4f0499db98ebbe4c2bb0
derek19960312 <46845914+derek19960312@users.noreply.github.com>
2019-04-30 00:39:03 +0800

    0430

---
+++ src/GrabCourseWS.java
@@ -0,0 +1,61 @@
+
+
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import com.google.gson.Gson;
+import com.websocketchat.model.ChatMessage;
+import com.websocketchat.model.State;
+
+
+
+@ServerEndpoint("/GrabCourseWS/{userName}")
+public class GrabCourseWS {
+	private static final Set<Session> connectedSessions = Collections.synchronizedSet(new HashSet<>());
+	Gson gson = new Gson();
+
+	@OnOpen
+	public void onOpen(@PathParam("userName") String userName, Session userSession) throws IOException {
+		connectedSessions.add(userSession);
+		String text = String.format("Session ID = %s, connected; userName = %s", userSession.getId(), userName);
+		System.out.println(text);
+	}
+
+	@OnMessage
+	public void onMessage(Session userSession, String message) {
+		for (Session session : connectedSessions) {
+			if (session.isOpen() && !userSession.getId().equals(session.getId()))
+				session.getAsyncRemote().sendText(message);
+		}
+		System.out.println("Message received: " + message);
+	}
+
+	@OnError
+	public void onError(Session userSession, Throwable e) {
+		System.out.println("Error: " + e.toString());
+	}
+
+	@OnClose
+	public void onClose(Session userSession, CloseReason reason) {
+		connectedSessions.remove(userSession);
+		String text = String.format("session ID = %s, disconnected; close code = %d; reason phrase = %s",
+				userSession.getId(), reason.getCloseCode().getCode(), reason.getReasonPhrase());
+		System.out.println(text);
+	}
+}
